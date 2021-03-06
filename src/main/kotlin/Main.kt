cad3bb644d5adfc0aa73f96aa0b7bc848113655b
Diushaliev Nursultan <nursd27@gmail.com>
2021-03-06 16:19:40 +0300

    adding test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
-import models.Question
 import org.springframework.context.support.ClassPathXmlApplicationContext
+import services.QuestionCSVDisplayService
 import services.QuestionCSVService
 
 fun main() {
@@ -7,18 +7,7 @@ fun main() {
 
     val questionCSVService = context.getBean(QuestionCSVService::class.java)
     val questions = questionCSVService.getQuestions()
-    displayQuestions(questions)
-}
 
-private fun displayQuestions(questions: List<Question>) {
-
-    for(i in 0 until questions.size) {
-        println(questions[i].description)
-        for(j in 0 until questions[i].answers.size) {
-            print(questions[i].answers[j].description)
-        }
-
-        println()
-        println()
-    }
+    val questionCSVDisplayService = context.getBean(QuestionCSVDisplayService::class.java)
+    questionCSVDisplayService.displayQuestions(questions)
 }
